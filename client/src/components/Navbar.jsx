5546d04fdff95ce8ebf5f9ebbc11acc38fb32df4
vickyshelar96k <vickyshelar96k@gmail.com>
2025-06-04 20:14:02 +0530

    Add responsive and styled Navbar with links to Home, About, Contact

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Navbar.css";
 
 const AppNavbar = () => (
-  <Navbar bg="dark" variant="dark" style={{color:"white"}}>
-    <ul>
-        <li><Link to="/" className="nav-link">Home</Link></li>
-        <li><Link to="/about" className="nav-link">About</Link></li>
-        <li></li>
-        <li></li>
-    </ul>
-  </Navbar>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container-fluid">
+      <Link className="navbar-brand logo-name" to="/">📝 Task Manager</Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ms-auto navdiv">
+          <li className="nav-item">
+            <Link className="nav-link text-info titlename" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link titlename" to="/about">About Me</Link>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link titlename" href="https://github.com/vickyshelar96k/Task-Manager" target="_blank" rel="noreferrer">GitHub</a>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link titlename" to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 );
 
 export default AppNavbar;
